ed3d043896d3a48418b80cf9003049c29d09048d
Al-shehabi91 <alshehabi91@yahoo.com>
2022-05-17 02:42:59 +0200

    branching2.py

---
+++ branching2.py
@@ -0,0 +1,9 @@
+input1 = input ("write the first number: ")
+input2 = input ("write the second number: ")
+input3 = input ("write the third number: ")
+
+print("")
+print("first output: ", float(input1) * float (input2))
+print("second output: ", float (input2) * float(input3))
+print("third output: ", float (input1) / float (input2))
+print("")
